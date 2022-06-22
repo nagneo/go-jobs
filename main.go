547d4bd2fd5ff5189ae552159f47f014428d1f5a
SaenaKIM <tosk3927@gmail.com>
2022-06-22 21:38:39 +0900

    [TIL] function - return multiple

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func multiply(a, b int) int {
-	return a * b
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
 }
 
 func main() {
-	fmt.Println(multiply(2, 3))
+	fmt.Println(lenAndUpper("nico"))
 }
