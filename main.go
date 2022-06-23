25f6a7567c97d716350c7dd85feb64152c7a4392
SaenaKIM <tosk3927@gmail.com>
2022-06-23 20:43:32 +0900

    delete content for new learning 2

--- main.go
+++ main.go
@@ -1,27 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/nagneo/go-jobs/mydict"
-)
-
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	word := "hello"
-	definition := "Greeting"
-	err := dictionary.Add(word, definition)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = dictionary.Delete(word)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	_, err = dictionary.Search(word)
-	if err != nil {
-		fmt.Println(err)
-	}
 }
