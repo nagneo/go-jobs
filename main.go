3dde85bce8ed5379b70a0eed8a9a10631e785a7b
SaenaKIM <tosk3927@gmail.com>
2022-06-25 19:41:37 +0900

    [TIL ]general exercist to hit url

--- main.go
+++ main.go
@@ -1,4 +1,48 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("Error for request")
+
 func main() {
+	results := map[string]string{}
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.naver.com/",
+		"https://www.yahoo.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for _, url := range urls {
+		result := "OK"
+		err := hitUrl(url)
+		if err != nil {
+			results[url] = "Failed"
+		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitUrl(url string) error {
+	fmt.Println("Checking: ", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
 }
