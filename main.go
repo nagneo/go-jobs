71575145b19c3d596137e8797bab8422e82e4162
SaenaKIM <tosk3927@gmail.com>
2022-06-25 23:01:38 +0900

    [TIL] start to web scrapping

--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
 var baseURL string = "https://kr.indeed.com/jobs?=q=python&limit=50"
 
 func main() {
 	pages := getPages()
+	fmt.Println(pages)
 }
 
 func getPages() int {
-	res, err := http.get(baseURL)
+	res, err := http.Get(baseURL)
 	checkErr(err)
-	
+	checkCode(res)
 
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination")
+
+	return 0
 }
-
 
 func checkErr(err error) {
 	if err != nil {
 		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with status:", res.StatusCode)
 	}
 }
