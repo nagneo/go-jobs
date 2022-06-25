84653dbccd4d8397e828f908aa28ddead4ad7941
SaenaKIM <tosk3927@gmail.com>
2022-06-25 22:13:26 +0900

    ignore .vscode

--- main.go
+++ main.go
@@ -1,55 +1,21 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"net/http"
-)
-
-type result struct {
-	url    string
-	status string
-}
-
-var errRequestFailed = errors.New("Error for request")
+var baseURL string = "https://kr.indeed.com/jobs?=q=python&limit=50"
 
 func main() {
-	results := make(map[string]string)
-	c := make(chan result)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.naver.com/",
-		"https://www.yahoo.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	}
-
-	for _, url := range urls {
-		go hitUrl(url, c)
-	}
-
-	// Blocking Operation
-	for i := 0; i < len(urls); i++ {
-		result := <-c
-		results[result.url] = result.status
-	}
-
-	for url, status := range results {
-		fmt.Println(url, status)
-	}
+	pages := getPages()
 }
 
-// <-: send only
-func hitUrl(url string, c chan<- result) {
-	resp, err := http.Get(url)
-	status := "OK"
-	if err != nil || resp.StatusCode >= 400 {
-		c <- result{url: url, status: "FAILED"}
+func getPages() int {
+	res, err := http.get(baseURL)
+	checkErr(err)
+	
+
+}
+
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
 	}
-	c <- result{url: url, status: status}
 }
